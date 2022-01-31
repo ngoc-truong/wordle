5f28eba4fd4c047689313060b20497037ac611af
Ngoc Truong <ngoc.truong@posteo.de>
2022-01-31 21:01:08 +0100

    Add minimal feedback

--- src/App.js
+++ src/App.js
@@ -4,9 +4,24 @@ function App() {
   const [targetWord, setTargetWord] = useState("eimer");
   const [guess, setGuess] = useState("");
   const [numOfGuesses, setNumOfGuesses] = useState(0);
+  const [feedback, setFeedback] = useState("");
 
   const handleWordSubmit = (event) => {
     event.preventDefault();
+
+    let letterFeedback = "";
+
+    for (let i = 0; i < guess.length; i++) {
+      if (guess[i].toLowerCase() === targetWord[i]) {
+        setFeedback((letterFeedback += "green "));
+      } else if (targetWord.includes(guess[i].toLowerCase())) {
+        setFeedback((letterFeedback += "yellow "));
+      } else {
+        setFeedback((letterFeedback += "x "));
+      }
+    }
+
+    setFeedback(letterFeedback);
   };
 
   const changeGuess = (event) => {
@@ -22,6 +37,7 @@ function App() {
         Your word: <input value={guess} onChange={changeGuess}></input>
         <button>Submit</button>
       </form>
+      <h2>{feedback}</h2>
     </div>
   );
 }
